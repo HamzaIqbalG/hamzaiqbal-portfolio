795b20bc7074b6b5eb7ead45d93c7ddd572fdcb0
hamzaiqbal10 <20hi4@queensu.ca>
2023-10-12 09:57:55 -0400

    slideshow issue fixed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 var currentImage = 0;
 var images = [
   "extra_images/welcomeMessage.jpg",
-  "extra_images/WelcomeMessage2.jpg"
+  "extra_images/welcomeMessage2.jpg"
 ];
 var slideInterval;
 var isPaused = false;
@@ -12,54 +12,21 @@ var isPaused = false;
 function slideImage() {
   var img = document.getElementById("slideshow-image");
   if (!img) return; // Check if the element exists
+  
+  img.style.transform = "translateX(-100%)";
+  img.style.transition = "transform 1s ease-in-out";
 
-  // Log the current image index and the corresponding image source
-  console.log('Current image index:', currentImage);
-  console.log('Current image source:', images[currentImage]);
-
-  img.src = images[currentImage];
-  img.onload = function() {
-      console.log('Image loaded:', images[currentImage]);
-  };
-  img.onerror = function() {
-      console.error('Error loading image:', images[currentImage]);
-  };
-
-  // Increment the index for the next image
-  currentImage = (currentImage + 1) % images.length;
+  setTimeout(function() {
+    img.src = images[currentImage];
+    img.style.transform = "translateX(0)";
+    currentImage = (currentImage + 1) % images.length;
+  }, 1000);
 }
 
 function startSlideShow() {
-  // Initialize slideInterval with a call to slideImage
+  slideImage();
   slideInterval = setInterval(slideImage, 5000);
-  slideImage();  // Call slideImage once to initialize the slideshow
 }
-
-
-
-
-
-
-
-
-// function slideImage() {
-//   var img = document.getElementById("slideshow-image");
-//   if (!img) return; // Check if the element exists
-  
-//   img.style.transform = "translateX(-100%)";
-//   img.style.transition = "transform 1s ease-in-out";
-
-//   setTimeout(function() {
-//     img.src = images[currentImage];
-//     img.style.transform = "translateX(0)";
-//     currentImage = (currentImage + 1) % images.length;
-//   }, 1000);
-// }
-
-// function startSlideShow() {
-//   slideImage();
-//   slideInterval = setInterval(slideImage, 5000);
-// }
 
 function pauseSlideShow() {
   var pauseButton = document.getElementById("pause-button");
